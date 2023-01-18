ba078b63f0763a0fce537f86e528ccd05a9d9e77
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-01-18 09:30:42 -0300

    feat: general information section finished

--- src/pages/Home/components/SectionGeneralInformation/styles.ts
+++ src/pages/Home/components/SectionGeneralInformation/styles.ts
@@ -3,12 +3,15 @@ import { styled } from '../../../../config/stitches.config';
 export const ContainerGeneralInformation = styled('div', {});
 
 export const ContentGeneralInformation = styled('div', {
-  padding: 20,
+  padding: 40,
+  margin: 20,
+  borderRadius: 5,
+  gap: 10,
+
   display: 'flex',
   flexWrap: 'wrap',
   alignItems: 'center',
   justifyContent: 'center',
-  gap: 10,
+
   background: '$backgroundLight',
-  width: 'auto',
 });
--- src/pages/Home/components/SectionServices/index.tsx
+++ src/pages/Home/components/SectionServices/index.tsx
@@ -20,7 +20,7 @@ export function SectionServices() {
       icon: <CodeIcon />,
     },
     {
-      title: 'Stacks',
+      title: 'Habilidades',
       text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
       icon: <StackIcon />,
     },
